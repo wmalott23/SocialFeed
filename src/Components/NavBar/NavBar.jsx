fc50c846a8ab762437fd340278b32c22fe46f6cb
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-22 13:43:40 -0700

    Added media query for 400-570 size screens

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,8 +2,8 @@ import "./NavBar.css"
 
 const NavBar = (props) => {
     return ( 
-      <div className="d-flex flex-row col-md-12 p-0">
-        <h3 className="title d-flex align-items-start">Social
+      <div className="nav-title d-flex flex-row col-md-12 p-0">
+        <h3 className="title d-flex align-items-start m-0">Social
         <h3 className="title-two text-muted">Feed</h3>
         </h3>
       </div>
